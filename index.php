9c29f602c30e04a308a1fab0d54034c8669d6c00
Sannic1998 <abobinaproductions@gmail.com>
2025-12-04 21:04:34 -0300

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AssociaDouga - Site dos Associados</title>
+</head>
+<body>
+    <p>Texto Básico só para visualizar</p>
+</body>
+</html>
