096776e4210cc0cf859c9f0c29755c5674ea809f
Vinnic1998 <abobinaproductions@gmail.com>
2025-12-04 21:33:15 -0300

    Commit 2.0

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AssociaDouga - Site dos Associados</title>
+    <link rel="icon" type="image/png" href="imagens/Favicon.png">
 </head>
 <body>
     <p>Texto Básico só para visualizar</p>
